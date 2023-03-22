acbaf5d2a50276bf64a83fc0516ed8a8d7761592
Лактионов Константин <klaktionov@kr.digital>
2023-03-22 05:46:47 +0300

    add password hashing add auth method

--- app/src/Controller/Action/RegisterAction.php
+++ app/src/Controller/Action/RegisterAction.php
@@ -7,20 +7,25 @@ namespace App\Controller\Action;
 use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class RegisterAction extends AbstractController
 {
-    public function __construct(private readonly ValidatorInterface $validator)
-    {
+    public function __construct(
+        private readonly ValidatorInterface $validator,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
     }
 
     public function __invoke(User $data): User
     {
         $validationErrors = $this->validator->validate($data);
-        if (count($validationErrors) > 0) {
+        if ($validationErrors->count() > 0) {
             throw new ValidationException($validationErrors);
         }
+
+        $data->setPassword($this->passwordHasher->hashPassword($data, $data->getPassword()));
 
         return $data;
     }
